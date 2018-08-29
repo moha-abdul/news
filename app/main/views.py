d75a0c00212161987d0e1c18c3a6b3fe53233ea7
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-29 20:23:36 +0300

    Create articles and article routes in view

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,9 @@ from flask import render_template
 from app import app
 
 # Views
+
+# index - sources route that will have the article sources list
+
 @app.route('/')
 def index():
 
@@ -11,4 +14,23 @@ def index():
 
     message = 'News API .'
     return render_template('index.html',message = message)
-    
+
+# View article route that will have the article lists in each source
+
+@app.route('/articles')
+def articles(article_id):
+
+    '''
+    View article sources page function that returns the list of articles from that source
+    '''
+    return render_template('articles.html',id = article_id)
+
+# View individual article, its posting time and author.
+
+@app.route('/article')
+def article(article_id):
+
+    '''
+    View article function that returns the article and author
+    '''
+    return render_template('article.html',id = article_id)
