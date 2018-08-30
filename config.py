f092411a91008250ae80b18e0b937f4bb7737d97
moha-abdul <mohammed.abdulla0602@gmail.com>
2018-08-30 08:55:45 +0300

    Add config, devconfig and prod config classes to news/config. set debug to true

--- config.py
+++ config.py
@@ -1,1 +1,29 @@
-NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+# NEWS_API_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    pass
+
+
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
